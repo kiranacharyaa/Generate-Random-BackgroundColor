a1e60c5d4d30b6e41a3ebbd31f39180a2a4b515d
kiranacharyaa <kiranacharya287@gmail.com>
2020-07-30 23:53:01 +0530

    First step of the code

---
+++ script.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector(".btn");
+let changecolor = document.getElementById("change-color");
+let body = document.querySelector("body");
+
+let colors = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"];
+
+function changeColor(){
+    let hexcolor = "#";
+    for(i=0;i<6;i++){
+        hexcolor+= colors[Math.floor(Math.random() * colors.length)];
+    }
+    console.log(hexcolor);
+    body.style.backgroundColor = hexcolor;
+    changecolor.innerHTML = hexcolor;
+}
