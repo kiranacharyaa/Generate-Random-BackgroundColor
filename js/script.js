3ad793c1f650d80abe9d958dd99c82a17828a9e2
kiranacharyaa <kiranacharya287@gmail.com>
2020-11-18 21:06:05 +0530

    added few more functions to change and copy color

--- js/script.js
+++ js/script.js
@@ -1,14 +1,42 @@
-let btn = document.querySelector(".btn");
-let changecolor = document.getElementById("change-color");
-let body = document.querySelector("body");
+const body = document.querySelector("body");
+const colorInput = document.querySelector("#color-input");
+const changeColorBtn = document.querySelector("#change-color");
+const copyColorBtn = document.querySelector("#copy-color");
+const eventMessage = document.querySelector(".event-message");
+const form = document.querySelector("form");
+let hexcolor;
 
-let colors = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"];
+const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
 
-function changeColor(){
-    let hexcolor = "#";
-    for(i=0;i<6;i++){
-        hexcolor+= colors[Math.floor(Math.random() * colors.length)];
-    }
-    body.style.backgroundColor = hexcolor;
-    changecolor.innerHTML = hexcolor;
+function changeColor() {
+    hexcolor = "#";
+	for (i = 0; i < 6; i++) {
+		hexcolor += colors[Math.floor(Math.random() * colors.length)];
+	}
+	body.style.backgroundColor = hexcolor;
+	colorInput.value = hexcolor;
+    eventMessage.innerHTML = "";
 }
+changeColorBtn.addEventListener("click", changeColor);
+
+function copyColor() {
+	colorInput.select();
+	document.execCommand("copy");
+	eventMessage.innerHTML = `<b>${colorInput.value}</b> Copied!`;
+}
+copyColorBtn.addEventListener("click", copyColor);
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    eventMessage.innerHTML = "";
+    if(colorInput.value.includes("#")){
+        hexcolor = "";
+    }else{
+        hexcolor = "#";
+    }
+    hexcolor += colorInput.value;
+    body.style.backgroundColor = hexcolor;
+    if(colorInput.value.length >= 9){
+        eventMessage.innerHTML = "Please enter valid code";
+    }
+})
